f195ef0e656c69d46872c43bd09542e0d342e483
stevenslxie <stevenslxie@gmail.com>
2014-09-21 16:03:13 +0200

    a bit tidy up with the receiver selection

--- examples/sky/tx-adjust.c
+++ examples/sky/tx-adjust.c
@@ -29,18 +29,12 @@
  *
  * -----------------------------------------------------------------
  *
- * Author  : Adam Dunkels, Joakim Eriksson, Niclas Finne
- * Created : 2006-03-07
- * Updated : $Date: 2010/01/15 10:32:36 $
- *           $Revision: 1.6 $
+ * Author  : Xie Shuanglong (stevenslxie@gmail.com)
+ * Created : 2014-09-26
+ * 
  *
- * Simple application to indicate connectivity between two nodes:
+ * An application to automatically find the optimum TX power with respect to each receiver 
  *
- * - Red led indicates a packet sent via radio (one packet sent each second)
- * - Green led indicates that this node can hear the other node but not
- *   necessary vice versa (unidirectional communication).
- * - Blue led indicates that both nodes can communicate with each
- *   other (bidirectional communication)
  *  
  *  Based on the whether the other node can hear the message, increase or decrease the tx power.
  */
@@ -55,8 +49,8 @@
 
 #include "net/mac/cc2420-pct.h"
 
-PROCESS(radio_test_process, "Radio test");
-AUTOSTART_PROCESSES(&radio_test_process);
+PROCESS(power_adjust_process, "TX Power Adjustment");
+AUTOSTART_PROCESSES(&power_adjust_process);
 
 
 #define HEADER "RTST"
@@ -69,11 +63,12 @@ AUTOSTART_PROCESSES(&radio_test_process);
 static uint8_t u8_0[MAX_NEIGHBORS*CYCLE] = {0};
 static uint8_t head_ptr = 0;
 static uint8_t split = 0;
+static uint8_t ID = 1;
+static uint8_t dest[MAX_NEIGHBORS-1] = {2,3};
+static uint8_t node_ptr = 0;
 
 struct pct_list{
 	uint8_t list[MAX_NEIGHBORS*CYCLE];
-    //uint8_t sender_ID;
-    //uint8_t recv_ID;
 };
 
 static struct etimer send_timer, delete_timer;
@@ -85,12 +80,10 @@ static uint8_t
 get_next(uint8_t *pos){
 	if(*pos < MAX_NEIGHBORS*CYCLE-1){
 		(*pos)++;
-		//return node_ptr;
 	}
     else
 	{
 		*pos = 0;
-		//return 0;
     }
 	return *pos;
 } 
@@ -98,7 +91,7 @@ get_next(uint8_t *pos){
 
 /*---------------------------------------------------------------------------*/
 static void
-abc_recv(struct unicast_conn *c, const rimeaddr_t *from)
+recv(struct unicast_conn *c, const rimeaddr_t *from)
 {
 
   if(0){
@@ -150,27 +143,24 @@ abc_recv(struct unicast_conn *c, const rimeaddr_t *from)
     //PROCESS_CONTEXT_END(&radio_test_process);
   }
 }
-static const struct unicast_callbacks abc_call = {abc_recv};
-static struct unicast_conn abc;
+static const struct unicast_callbacks recv_call = {recv};
+static struct unicast_conn pct;
 /*---------------------------------------------------------------------*/
-PROCESS_THREAD(radio_test_process, ev, data)
+PROCESS_THREAD(power_adjust_process, ev, data)
 {
-  //PROCESS_BEGIN();
 
-  //abc_open(&abc, PORT, &abc_call);
-
-  PROCESS_EXITHANDLER(unicast_close(&abc);)
+  PROCESS_EXITHANDLER(unicast_close(&pct);)
     
   PROCESS_BEGIN();
 
-  unicast_open(&abc, 146, &abc_call);  
+  unicast_open(&pct, 146, &recv_call);  
 
   etimer_set(&send_timer, CLOCK_SECOND);
-  etimer_set(&delete_timer,2*CLOCK_SECOND);
+  etimer_set(&delete_timer, CLOCK_SECOND);
 
   rimeaddr_t my_addr;
-
-  my_addr.u8[0] = 1;
+ 
+  my_addr.u8[0] = ID;
   my_addr.u8[1] = 0;
 
   rimeaddr_set_node_addr(&my_addr);
@@ -195,25 +185,21 @@ PROCESS_THREAD(radio_test_process, ev, data)
 		}
 		printf("\n");
 
-		//send_pac->sender_ID = rimeaddr_node_addr.u8[0];
+		node_ptr++;
 
-		if(0==split){
-            to_addr.u8[0] = 3;
-            to_addr.u8[1] = 0;
-			//send_pac->recv_ID = to_addr.u8[0];	
-			split = 1;
-		}else{
-            to_addr.u8[0] = 2;
-            to_addr.u8[1] = 0;
-			//send_pac->recv_ID = to_addr.u8[0];
-			split = 0;
-		}
+		if(node_ptr>(MAX_NEIGHBORS-2))
+			node_ptr = 0;
+
+
+		to_addr.u8[0] = dest[node_ptr];
+        to_addr.u8[1] = 0;
+
     	//printf("the sender ID prepared is:%u.\n",send_pac->sender_ID);
 
 		cc2420_set_txpower((uint8_t)(get_adjusted_tx_power(to_addr.u8[0])));
 
 		if(!rimeaddr_cmp(&to_addr, &rimeaddr_node_addr)) {
-      		unicast_send(&abc, &to_addr);
+      		unicast_send(&pct, &to_addr);
     	}
 		printf("Sending a packet.\n");
 
